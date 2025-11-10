46ff158f268c944fac1f473d8bd1b3af0bb6d5a4
Filip Sondej <26285707+filyp@users.noreply.github.com>
2025-11-10 20:50:05 +0100

    use screen number 0 everywhere

--- classes/screen.py
+++ classes/screen.py
@@ -4,7 +4,7 @@ from psychopy import event, logging, visual
 from screeninfo import get_monitors
 
 
-def get_screen_res(screen_number=-1):
+def get_screen_res(screen_number=0):
     """
     Funcion that check current screen resolution. Raise OSError if can't recognise OS!
     * :return: (width, height) tuple with screen resolution.
